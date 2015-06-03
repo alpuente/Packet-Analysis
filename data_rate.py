1532c88adf62d23a8693add28247998c6bf6960b
Jimmy Klein <jimmy.klein4@gmail.com>
2015-06-03 10:35:27 -0400

    Now shows total time

--- data_rate.py
+++ data_rate.py
@@ -26,34 +26,39 @@ else:
    f = open(name, 'r')
 
 contents = f.read()
-qosData = re.compile('QoS Data.{,}IEEE', re.DOTALL)
+qosData = re.compile('QoS Data.{,}Data \(', re.DOTALL)
 rate = re.compile('Data Rate: [0-9]*\.[0-9]')
 dataRate = re.compile('[0-9]+ bytes on wire')
+epochTime = re.compile('Epoch Time: [0-9]+\.[0-9]+')
 
 allQoS = qosData.findall(contents)
 joinedAllQoS = " ".join(allQoS)
 allRates = rate.findall(joinedAllQoS)
 allData = dataRate.findall(joinedAllQoS)
+allTime = epochTime.findall(joinedAllQoS)
 
 l =[]
 p = []
-
+m = []
 print 'Total packets: ' + str(len(allRates))
 
 for x in range(len(allRates)):
-    for t in allRates[x].split():
-        try:
-            l.append(float(t))
-        except ValueError:
-            pass
-        
-    for r in allData[x].split():
-        try:
-            p.append(float(r))
-        except ValueError:
-            pass
+  for t in allRates[x].split():
+    try:
+      l.append(float(t))
+    except ValueError:
+      pass
+  for r in allData[x].split():
+    try:
+      p.append(float(r))
+    except ValueError:
+      pass
+  for q in allTime[x].split():
+    try:
+      m.append(float(q))
+    except ValueError:
+      pass
 
 print "Total data transmitted (MB): " + str(sum(p)/(1024*1024))
-
 print "Average data rate (Mb/s): " + str(sum(l)/float(len(l)))
-
+print "Time taken: " + str(m[-1]-m[0])
